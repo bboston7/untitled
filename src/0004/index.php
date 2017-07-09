0da4fc42436f40f6cda62de26f40a64eb44f2516
Brett Boston <boston@csail.mit.edu>
2017-07-09 14:51:57 -0400

    0004

---
+++ src/0004/index.php
@@ -0,0 +1,14 @@
+<?php
+  require("../common.php");
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <p>The whole thing really does defy description.  But in four words:</p>
+    <p><a href=<?=$linky?>>going into the page</a></p>
+  </body>
+</html>
